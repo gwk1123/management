261bd650c709c492288838b5629a1132f1b18abf
gwk1123 <g15625281551@163.com>
2020-10-03 10:42:03 +0800

    缓存

--- src/main/java/com/ry/manage/ManageApplication.java
+++ src/main/java/com/ry/manage/ManageApplication.java
@@ -4,12 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 
+@EnableFeignClients(basePackages = { "comm.feign"})
 @SpringBootApplication
 @MapperScan({"com.ry.manage.sys.mapper","comm.repository.mapper"})
-@ComponentScan({"com.ry.manage","comm.utils.redis.impl","comm.config"})
+//@ComponentScan({"com.ry.manage","comm.utils.redis.impl","comm.config","comm.runner"
+//,"comm.repository.mapper"})
+@ComponentScan({"com.ry.manage","comm.utils","comm.config","comm.runner"
+        ,"comm.repository.mapper","comm.service"})
 public class ManageApplication {
 
     public static void main(String[] args) {
