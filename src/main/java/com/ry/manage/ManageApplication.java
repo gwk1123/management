ab75ff4505657089058883e7b55604580f4432cf
gwk1123 <g15625281551@163.com>
2020-11-13 16:51:29 +0800

    注册服务中心

--- src/main/java/com/ry/manage/ManageApplication.java
+++ src/main/java/com/ry/manage/ManageApplication.java
@@ -3,10 +3,11 @@ package com.ry.manage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-
+@EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.sibecommon.feign"})
 @SpringBootApplication
 @MapperScan({"com.ry.manage.sys.mapper","com.sibecommon.repository.mapper"})
--- src/main/java/com/ry/manage/task/DomeXxlJob.java
+++ src/main/java/com/ry/manage/task/DomeXxlJob.java
@@ -3,6 +3,8 @@ package com.ry.manage.task;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import com.xxl.job.core.log.XxlJobLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -10,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class DomeXxlJob {
 
+    private Logger logger = LoggerFactory.getLogger(DomeXxlJob.class);
 
     /**
      * 1、简单任务示例（Bean模式）
@@ -18,6 +21,7 @@ public class DomeXxlJob {
     public ReturnT<String> demoJobHandler(String param) throws Exception {
         XxlJobLogger.log("XXL-JOB, Hello World.");
 
+        logger.info("开始执行----->");
         for (int i = 0; i < 5; i++) {
             XxlJobLogger.log("beat at:" + i);
             TimeUnit.SECONDS.sleep(2);
